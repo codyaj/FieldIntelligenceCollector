d2e5759f9db6da9de8e30d84c9cef0c67a2e1265
CodyJ <cajackson05@icloud.com>
2025-09-12 00:05:01 +1000

    Add sidebar

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'dashboard', component: Home},
+    { path: 'analytics', component: Home},
+    { path: 'settings', component: Home},
+    { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+];
