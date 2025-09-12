b7e083de445bc8cdc71b83cab9f6eadb448c305a
CodyJ <cajackson05@icloud.com>
2025-09-12 22:36:29 +1000

    Add Dashboard UI

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
+import { Analytics } from './analytics/analytics';
+import { Settings } from './settings/settings';
 
 export const routes: Routes = [
     { path: 'dashboard', component: Home},
-    { path: 'analytics', component: Home},
-    { path: 'settings', component: Home},
+    { path: 'analytics', component: Analytics},
+    { path: 'settings', component: Settings},
     { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
 ];
--- frontend/src/app/home/home.ts
+++ frontend/src/app/home/home.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
 export class Home {
-
+  ssids: string[] = ['Network1', 'HomeWiFi', 'Guest', 'Office', 'Network1', 'HomeWiFi', 'Guest', 'Office', 'Network1', 'HomeWiFi', 'Guest', 'Office'];
+  deviceType: { type: string; amount: number }[] = [
+    { type: 'Camera', amount: 10 },
+    { type: 'TV', amount: 4 },
+    { type: 'Phone', amount: 13 },
+    { type: 'Computer', amount: 2 },
+  ];
 }
